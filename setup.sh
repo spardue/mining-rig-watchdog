0fad191c220db35756724213511244d11d16d0e7
spardue <pardue.stephen@gmail.com>
2018-01-29 23:55:03 +0000

    setup doesn't check for existance of config file. watchdog logs when it resets, and renamed menu to ctrl

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 		
 
-if [[ -f "config" ]]; then
 WALLET_ADDRESS=$(whiptail --inputbox "Please enter your ethereum wallet address" 8 78 --title "Setup" 3>&1 1>&2 2>&3)
 WATCHDOG_HOME=$(whiptail --inputbox "Please enter directory for watchdog home" 8 78 --title "Setup" 3>&1 1>&2 2>&3)
-fi
 echo "WATCH_DOG_HOME=$WATCHDOG_HOME" > config
 echo "ETH_WALLET=$WALLET_ADDRESS" >> config
